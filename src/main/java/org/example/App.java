1517e0473bb27b2f446b21d47137cbeeaf6aa47f
Aelfric Mayer <aelfric.mayer@faktorzehn.at>
2023-08-07 16:03:20 +0200

    test commit for jenkins trigger

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,44 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * TODO: Build jenkins Job with build trigger on push
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+
+        int mode = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(calculatorApp(mode, a, b));
+    }
+
+    static int calculatorApp(int mode, int a, int b) {
+        switch(mode) {
+            case 1:
+                return add(a, b);
+            case 2:
+                return multiply(a, b);
+            case 3:
+                return divide(a, b);
+            default:
+                return 0;
+        }
+    }
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    static int divide(int a, int b) {
+        return a / b;
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -8,31 +8,31 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
-{
+    extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
+    public AppTest( String testName ) {
         super( testName );
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
+    public static Test suite() {
         return new TestSuite( AppTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        int a = 5;
+        int b = 5;
+        int result = App.calculatorApp(1, a, b);
+
+        assertEquals(result, 10);
     }
 }
